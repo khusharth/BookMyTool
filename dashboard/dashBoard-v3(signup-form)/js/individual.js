cad372f39e477b34f69cf69df6ea8b452181f9f8
Khusharth <khusharth19@gmail.com>
2019-08-24 10:52:29 +0530

    bug fix for chrome in individual.html

--- dashboard/dashBoard-v3(signup-form)/js/individual.js
+++ dashboard/dashBoard-v3(signup-form)/js/individual.js
@@ -2,8 +2,9 @@
 
 	// On selecting Other Show Other input field
 	$('#other-input').hide();
-	$('#other-select').click(function() {
-	    $('#other-input').removeClass('collapse');
+	$("#other-select").click(function() {
+	    $('#other-input').css("display","block");
+	    $("#number-input").css("background-color","red");
 	    // $("#number-input").before($("#other-input"));
 	});
 
@@ -11,3 +12,13 @@
 	$('.on-select-hide').click( function() {
 	    $('#other-input').addClass('collapse');
 	});
+
+	$('#select-industry').on('change', 'select', function() {
+		var index = $(this).find('option:selected').index();
+		if (index == 4) {
+			$('#other-input').show();
+		} else {
+			$('#other-input').hide();
+		}
+	  });
+	  
